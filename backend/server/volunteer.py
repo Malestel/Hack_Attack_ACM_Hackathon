bbc2083a87c6ce4a87084eb21711f8c96d3578c0
Stephen Wight <srwight@gmail.com>
2022-03-26 12:23:47 -0500

    implement new volunteer and update volunteer

--- backend/server/volunteer.py
+++ backend/server/volunteer.py
@@ -21,20 +21,26 @@ def new_volunteer():
     #     abort(405, "Time not available")
     appt = request.json
 
+    result = db.create_volunteer(**appt)
+
     ret_payload = dict(
-        location=BASE + f'/{id}'
+        location=BASE + f'/{result}'
     )
-    return jsonify(ret_payload), 200
+    if result > 0:
+        return jsonify(ret_payload), 200
+    else:
+        abort(500, "Error creating volunteer")
 
 
-@app.route(BASE + '/<id>', methods=PUT)
-def update_volunteer(id):
-    # UPDATE VOLUNTEER IN DATABASE
-    # if id_not_found:
-    #     abort(404, "Volunteer not found")
+@app.route(BASE + '/<vol_id>', methods=PUT)
+def update_volunteer(vol_id):
+
+    vol = request.json
+
+    result = db.update_volunteer(vol_id, **vol)
 
     ret_payload = dict(
-        location=BASE + f'/{id}'
+        location=BASE + f'/{vol_id}'
     )
     return jsonify(ret_payload), 200
 
