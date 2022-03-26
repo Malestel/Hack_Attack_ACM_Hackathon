4011ad64b7d72fcae27bce4bb1b0a7ad92f03a2b
Stephen Wight <srwight@gmail.com>
2022-03-26 18:13:55 -0500

    begin to add login capability

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -9,6 +9,8 @@ db = DbApi()
 GET, PUT, POST, DELETE = ('GET',), ('PUT',), ('POST',), ('DELETE',)
 
 app = Flask(__name__)
+app.secret_key = b'4e621bd9807f0aa91af391157b6c12572cb5ff2255244f6cb74cb96f1d107d21'
+
 login_manager.init_app(app)
 
 
---
+++ backend/server/user.py
@@ -0,0 +1,40 @@
+from flask_login import UserMixin
+from . import db 
+
+class User(UserMixin):
+    @classmethod
+    def get(cls, user_id):
+
+        obj = cls()
+        
+        try:
+            user = db.get_web_user(user_id)
+        except KeyError:
+            return None
+
+        obj.Name = user['Name']
+        obj.id = user['Phone_Number'].encode()
+        obj.language = user['Language']
+        obj.user_type=user['user_type']
+
+        return obj
+    
+    def __getitem__(self, key):
+        if key == "Name":
+            return self.Name
+
+        if key == "Phone_Number":
+            return self.id
+        
+        if key == "Language":
+            return self.language
+
+        if key == "user_type":
+            return self.user_type
+
+        raise KeyError(f"Unknown key {key}")
+
+    def keys(self):
+        return ("Name", "Phone_Number", "Language", "user_type")
+
+
