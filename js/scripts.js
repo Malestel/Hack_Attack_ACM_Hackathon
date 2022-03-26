a419ecfd199c155c8436935f6ae11cd9fb8242c5
Carolyn K Savas <carolyn.savas.keith@gmail.com>
2022-03-26 14:46:29 -0500

    script

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,11 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+function storeUserType(x){
+    sessionStorage.setItem("usertype:", x);
+    console.log(x);
+    console.log(sessionStorage.getItem("usertype"));
+}
+
+document.getElementById('clickClient').addEventListener('change', storeUserType("client"), false);
+document.getElementById('clickVolunteer').addEventListener('change', storeUserType("volunteer"), false);
