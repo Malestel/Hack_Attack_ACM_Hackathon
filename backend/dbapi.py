ba2cfa2c3af45b9ecb79e70705e613e9a4c5d267
Stephen Wight <srwight@gmail.com>
2022-03-26 00:48:36 -0500

    implement all selects

--- backend/dbapi.py
+++ backend/dbapi.py
@@ -19,8 +19,48 @@ class DbApi:
     def _update_tables(self):
         self.metadata.reflect(self.db)
         self.tables = {table.name: table for table in self.metadata.sorted_tables}
+        
+    def _make_output(cols, vals):
+        return [dict(zip(cols, val) for val in vals)]
 
-    def get_volunteers(self):
-        out = self.db.execute('SELECT * FROM "Volunteers"')
-        return out
+    def get_volunteer(self, id=None):
+        self._update_tables()
+        table = self.tables['Volunteers']
+        q:Query = self.session.query(table)
+        
+        if id is not None:
+            q = q.filter(table.c.Vol_ID == id)
+        
+        cols = [col.name for col in table.c]
+        result = q.all()
+
+        return self._make_output(cols, result)
+
+    def get_user(self, name=None):
+        self._update_tables()
+        table = self.tables['Users']
+        q:Query = self.session.query(table)
+
+        if id is not None:
+            q = q.filter(table.c.Name == name)
+        
+        cols = [col.name for col in table.c]
+        result = q.all()
+
+        return self._make_output(cols, result)
+
+
+    def get_appointment(self, name):
+        self._update_tables()
+        table = self.tables['Appointment']
+        q:Query = self.session.query(table)
+
+        if id is not None:
+            q = q.filter(table.c.Name == name)
+
+        cols = [col.name for col in table.c]
+        result = q.all()
+
+        return self._make_output(cols, result)
+
         
