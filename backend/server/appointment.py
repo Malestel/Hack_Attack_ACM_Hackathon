08910954182f4a2b6926e11e5fe028689d756f7d
Stephen Wight <srwight@gmail.com>
2022-03-25 23:56:06 -0500

    add jsonify and abort

--- backend/server/appointment.py
+++ backend/server/appointment.py
@@ -8,37 +8,39 @@ BASE = '/api/appointment'
 def get_appointment(id=None):
     # GET APPOINTMENTS FROM DATABASE
     # if id_not_found:
-    #     return "Unknown appointment", 404
+    #     abort(404, "Appointment not found")
     appointments = []
-    return appointments, 200
+    return jsonify(appointments), 200
 
 @app.route(BASE, methods=POST)
 def new_appointment():
     # CREATE APPOINTMENT IN DATABASE
 
     # if time_conflict:
-    #     return "Time not available", 405
+    #     abort(405, "Time not available")
+
+    appt = request.json
 
     ret_payload = dict(
         location=BASE+f'/{id}'
     )
-    return ret_payload, 200
+    return jsonify(ret_payload), 200
 
 @app.route(BASE + '/<id>', methods=PUT)
 def update_appointment(id):
     # UPDATE APPOINTMENT IN DATABASE
     # if id_not_found:
-    #     return "Unknown appointment", 404
+    #     abort(404, "Appointment not found")
 
     ret_payload = dict(
         location=BASE+f'/{id}'
     )
-    return ret_payload, 200
+    return jsonify(ret_payload), 200
 
 @app.route(BASE + '/<id>', methods=DELETE)
 def delete_appointment(id):
     # DELETE APPOINTMENT IN DATABASE
     # if id_not_found:
-    #     return "Unknown appointment", 404
+    #     abort(404, "Appointment not found")
     
     return "OK", 200
