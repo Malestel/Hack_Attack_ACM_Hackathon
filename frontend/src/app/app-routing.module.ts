594f107b97ab72ec0feff9709484f14b311e6f9f
Stephen Wight <srwight@gmail.com>
2022-03-26 21:56:26 -0500

    add pre-vid to routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ChatComponent } from './chat/chat.component';
 import { ClientInfoComponent } from './client-info/client-info.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PreVideoComponent } from './pre-video/pre-video.component';
 
 const routes: Routes = [
   {path: '/', component: HomeComponent},
   {path: '/login', component: LoginComponent},
   {path: '/client-info', component: ClientInfoComponent},
-  {path: '/chat', component: ChatComponent}
+  {path: '/chat', component: ChatComponent},
+  {path: '/pre-vid', component: PreVideoComponent}
 ];
 
 @NgModule({
