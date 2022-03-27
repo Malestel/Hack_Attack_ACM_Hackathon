5a85d5fd323b60655fd4e6595c11388f7f580fe5
Stephen Wight <srwight@gmail.com>
2022-03-27 10:29:26 -0500

    remove space in route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PreVideoComponent } from './pre-video/pre-video.component';
 const routes: Routes = [
-  {path: ' ', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'client-info', component: ClientInfoComponent},
   {path: 'chat', component: ChatComponent},
